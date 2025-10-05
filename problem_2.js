94f6035dc62f7c8b2f7a1f16533b236b47369d0a
Uzar name <souravmondol135@gmail.com>
2025-10-05 12:29:34 +0600

    problem_2

---
+++ problem_2.js
@@ -0,0 +1,11 @@
+let num = -5;
+
+    if (num > 0) {
+        console.log("Positive");
+    }
+    else if (num < 0) {
+        console.log("Negative");
+    }
+    else {
+        console.log("Zero");
+    }
